f4dbb56baf6b3a38cf57a167add00c2984848958
Adam Gibbs <git@status203.com>
2025-09-14 18:43:34 +0100

    Basic calculation algo completed

--- src/trees/core.clj
+++ src/trees/core.clj
@@ -1,14 +1,32 @@
 (ns trees.core
-  (:require [quil.core :as q]))
+  (:require [quil.core :as q]
+            [trees.tree :as tree]))
+
+
+(def width 600)
+(def height 600)
+
+(defn downrightify-point
+  "Translates a point from origin bottom left to origin top left"
+  [[x y]]
+  [x (- y)])
 
 (defn setup []
-  (q/background 0xDD))
+  (q/background 0xDD)
+  (let [tree (tree/grow 200 90 {:another-child? tree/right-angle-child?
+                                :branch-angle   tree/right-angle-angle
+                                :branch-length  tree/halving-branches-length
+                                :children?      tree/smallest-branch-32-children?})]
+    (q/with-translation [(/ width 2) height]
+      (doseq [branch (tree-seq :children :children tree)]
+        (q/line (downrightify-point (:start branch)) 
+                (downrightify-point (:end branch)))))))
 
 (defn draw []
   nil)
 
 (q/defsketch trees
   :title "Fractal Trees"
-  :size  [600 600]
+  :size  [width height]
   :setup setup
   :draw  draw)
---
+++ src/trees/tree.clj
@@ -0,0 +1,106 @@
+(ns trees.tree)
+
+(defn right-angle-child?
+  [_parent siblings]
+  (boolean (> 2 (count siblings))))
+
+(defn right-angle-angle
+  [_parent siblings]
+  (let [ct (count siblings)]
+    (if (zero? ct) -45 90)))
+
+(defn halving-branches-length
+  "Each branch has a length of half its parent."
+  [parent _siblings]
+  (/ (:length parent) 2))
+
+(defn smallest-branch-32-children?
+  "Returns true if branch was larger than 32"
+  [_parent _siblings branch]
+  (> (:length branch) 32))
+
+;; See trees.branch/draw for the format of a branch
+
+(defn base-angle
+  "Takes a (growing) branch and it's chldren to date and returns the angle
+   from which the next child's branch should be calculated.
+   
+   Note that branch angles are calculated clockwise"
+  [parent siblings]
+  (println :got-here)
+  (if-let [sibling-angle (->> siblings last :abs-angle)]
+    sibling-angle
+    (:abs-angle parent)))
+
+(defn- convert-angle
+  "Takes a clockwise angle from West in degrees and converts it to an anti-clockwise
+   angle from East in radians"
+  [deg]
+  (-> deg
+      (mod 360)
+      Math/toRadians))
+
+(defn calc-end
+  [[s1 s2] degs length]
+  (let [rads (convert-angle degs)]
+    [(+ s1 (* length (Math/cos rads)))
+     (+ s2 (* length (Math/sin rads)))]))
+
+(defn grow
+  "opts (all are mandatory)
+     :branch-angle - [parents siblingls] -> degrees clockwise
+     called to calculate the relative angle from a branch's last sibling or from 
+     the parents angle if it's the first child.
+
+     :branch-length - [parents branch] -> length
+     called to calculate the length of the branch
+
+     :another-child? - [parents siblings] -> bool
+     called to decide whether to add another branch to siblings
+
+     :children? - [parents siblings branch] -> bool
+     called to decide whether the branch should have any children
+
+   A branch is a map of 
+     :start     [x y]
+     :end       [x y]
+     :rel-angle clockwise-degrees
+     :abs-angle clockwise-degrees-from-West
+     :length    length
+     :parent    branch
+     :children  [...branches]
+    
+   For now, assumes a single trunk (which is considered to be branch level 0)"
+  ([trunk-length trunk-angle opts]
+   (let [trunk {:start [0 0]
+                :end       (calc-end [0 0] trunk-angle trunk-length)
+                :rel-angle trunk-angle
+                :abs-angle trunk-angle
+                :length    trunk-length
+                :parent    nil
+                :children  nil}]
+     (grow trunk opts)))
+  ([parent {:keys [another-child? branch-angle branch-length children?] :as opts}]
+   (loop [siblings []]
+     (if (another-child? parent siblings)
+       (let [rel-angle (branch-angle parent siblings)
+             abs-angle (+ (base-angle parent siblings) rel-angle)
+             length    (branch-length parent siblings)
+             start     (:end parent)
+             end       (calc-end start abs-angle length)
+             base      {:start     start
+                        :end       end
+                        :rel-angle rel-angle
+                        :abs-angle abs-angle
+                        :length    length
+                        :parent    parent
+                        :children  nil}
+             child     (if (children? parent siblings base) (grow base opts) base)]
+         (recur (conj siblings child)))
+       (assoc parent :children siblings)))))
+
+(comment
+  (grow 200 90 {:another-child? right-angle-child?
+                :branch-angle   right-angle-angle
+                :branch-length  halving-branches-length
+                :children?      smallest-branch-32-children?}))
